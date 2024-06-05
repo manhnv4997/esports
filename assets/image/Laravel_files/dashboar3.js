b9634a92a937c6840645d805bc5b75c3f856689e
manhnv <manhnv4997@gmail.com>
2024-06-05 11:20:36 +0700

    Feature: add font

---
+++ assets/image/Laravel_files/dashboar3.js
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- IonIcons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+
+
+    
+    
+    
+
+
+    
+    
+
+
+
+
+    <!-- jQuery -->
+    <script src="http://nhatro.test/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="http://nhatro.test/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE -->
+    <script src="http://nhatro.test/assets/dist/js/adminlte.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="http://nhatro.test/assets/dist/js/pages/dashboar3.js"></script>
+
+    <script type="module" src="http://127.0.0.1:5173/@vite/client"></script><link rel="stylesheet" href="http://127.0.0.1:5173/resources/vue/app/assets/scss/app.scss" /><script type="module" src="http://127.0.0.1:5173/resources/vue/app/app.js"></script>
+</head>
+
+<body class=" sidebar-mini">
+    <div id="app"></div>
+    <div id="end-of-body"></div>
+</body>
+
+</html>
